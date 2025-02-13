8079395f64c7a8dbaf936ecc570a662280099832
José Salazar <jmsalazar.macg@hotmail.com>
2025-02-13 13:33:44 -0600

    main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,14 +11,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // left icon
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800],
-        ),
-        actions: [Icon(Icons.person)],
-      ),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.purple,
+            // left icon
+            leading: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Icon(Icons.person),
+              ),
+            ]),
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              children: [
+                Text("I want to ",
+                    style: TextStyle(
+                      fontSize: 32,
+                    )),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
